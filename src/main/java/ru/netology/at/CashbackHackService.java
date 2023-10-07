5cb0cee3ccec3acd102df2cb492e6ae087e26757
Olga <helga_ser@mail.ru>
2023-10-07 21:28:18 +0300

    Initial

---
+++ src/main/java/ru/netology/at/CashbackHackService.java
@@ -0,0 +1,10 @@
+package ru.netology.at;
+
+public class CashbackHackService {
+    private final int boundary = 1000;
+
+    public int remain(int amount) {
+        return boundary - amount % boundary;
+
+    }
+}
